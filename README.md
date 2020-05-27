7292a22dc0c1c6edaa75759a3819c8e06b29fd7b
Edebo <akandeadebowale0@gmail.com>
2020-05-27 16:40:20 +0100

    published code

---
+++ README.md
@@ -0,0 +1,5 @@
+**-Statistics distribution packag for Gaussian and Binomial distribution-**
+
+**-uploaded package to-**
+**test.pypi.org**
+https://test.pypi.org/project/dbdist/0.1/
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='dbdist',
+      version='0.1',
+      description='Gaussian and Normal distributions class',
+      packages=['dbdist'],
+      zip_safe=False)
